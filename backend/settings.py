a873abb51c31446ea92d37aae96ef656d31cd702
unknown <legacytild@gmail.com>
2023-07-22 18:19:12 +0300

    sss

--- backend/settings.py
+++ backend/settings.py
@@ -198,19 +198,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/build/static/'
 MEDIA_URL = '/images/'
+print('sssssssssssss', STATIC_URL)
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build', 'static'),
-]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = 'https://firebasestorage.googleapis.com/v0/b/nutrinationcloud.appspot.com/o/images'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-print(STATIC_ROOT, 'here is STATIC_ROOT')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
+print('sssssssssssss', STATIC_ROOT)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
